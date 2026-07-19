e6d061522df99cf7a9706f4f372582fcb3745ee4
NidhiPradh <nidhipradhan54@gmail.com>
2026-07-20 00:40:16 +0530

    states and district api added

---
+++ FinVerse.Infrastructure/models/DistrictEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinVerse.Infrastructure.models
+{
+    public class DistrictEntity
+    {
+        public int? DistrictId { get; set; }
+
+        public string? DistrictName { get; set; } = string.Empty;
+
+        public int? StateId { get; set; }
+
+        public int? CountryId { get; set; }
+    }
+}
---
+++ FinVerse.Infrastructure/models/StatesEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinVerse.Infrastructure.models
+{
+    public class StatesEntity
+    {
+        public int? StateId { get; set; }
+        public string? StateName { get; set; }
+        public int? CountryId { get; set; }
+    }
+}
